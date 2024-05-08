b343a237a3a27f49ec6149490f735df6acff56e6
Gyan-Ponnappa-1 <gyanopcon@gmail.com>
2024-05-08 19:57:34 +0530

    all set. afterLogin nav

--- app/src/main/java/com/example/kodavamatrimony/ui/Navigation/DestinationScreen.kt
+++ app/src/main/java/com/example/kodavamatrimony/ui/Navigation/DestinationScreen.kt
@@ -2,7 +2,6 @@ package com.example.kodavamatrimony.ui.Navigation
 
 sealed class DestinationScreen(var route : String) {
     data object SignUp:DestinationScreen("signup")
-    data object BottomNavigationMenu:DestinationScreen("bottomNavigationMenu")
     data object Login:DestinationScreen("login")
     data object HomeScreen:DestinationScreen("homeScreen")
     data object SplashScreen:DestinationScreen("splashScreen")
@@ -11,7 +10,6 @@ sealed class DestinationScreen(var route : String) {
     data object CreateProfileScreen:DestinationScreen("createProfileScreen")
     data object MyProfilesScreen:DestinationScreen("myProfilesScreen")
     data object ChatListScreen:DestinationScreen("chatListScreen")
-    data object AfterLoginScreen:DestinationScreen("afterLoginScreen")
     data object SingleProfileScreen:DestinationScreen("singleProfileScreen/{profileId}"){
        fun createRoute(id : String) = "singleProfileScreen/$id"
     }
